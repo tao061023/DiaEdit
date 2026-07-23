96a26cbc7034873c4bc5ea5bcdee4aa56b7bbc9e
tao061023 <moto23fuchi@icloud.com>
2026-07-23 15:49:06 +0900

    プロジェクトファイルを追加。

---
+++ DiaEditCore.Tests/SmorkTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DiaEditCore.Tests
+{
+    public class SmokeTest
+    {
+        [Fact]
+        public void Placeholder_ShouldPass()
+        {
+            // テスト基盤の疎通確認用。中身に意味はない。
+            Assert.Equal(2, 1 + 1);
+        }
+    }
+}
